30ea41768f559bb986d404c2099c4c314528bbbd
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-06-14 16:35:42 +0300

    feat : add fetch method into the main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod quote;
+use quote::Quote;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let quote = reqwest::get("https://dummyjson.com/quotes/random")
+        .await?
+        .json::<Quote>()
+        .await?;
+    quote.print();
+    Ok(())
 }
