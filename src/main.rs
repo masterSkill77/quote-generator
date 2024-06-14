69518bf347f585dcc063f6da74762e9d8b687bd4
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-06-14 19:45:47 +0300

    feat : update code to only make a request if no args present

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+mod cli;
 mod quote;
+
+use cli::{cli, print_author};
 use quote::fetch_random_quote;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let quote = fetch_random_quote().await?;
-    quote.print();
+    let args = cli().get_matches();
+    if !args.args_present() {
+        let quote = fetch_random_quote().await?;
+        quote.print();
+    }
+    if args.is_present("author") {
+        print_author();
+    }
+
     Ok(())
 }
