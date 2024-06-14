f1640a190b1dc64b241658f7eb208f784121c6fa
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-06-14 23:28:26 +0300

    fix : remove check for rust

--- install.sh
+++ install.sh
@@ -12,17 +12,6 @@ INSTALLATION_NAME="quote-generator.deb"
 
 ASSET_NAME="quote-generator_0.1.0-1_amd64.deb"
 
-# If Rust is not installed
-if ! command -v rustc >/dev/null 2>&1; then
-    echo "Rust is not installed, please install it"
-    exit 1
-fi
-
-if ! command -v cargo >/dev/null 2>&1; then
-    echo "Cargo is not installed, please install it"
-    exit 1
-fi
-
 # Determine Operating System
 DOWNLOAD_URL=$(curl -sSL $RELEASE_URL | grep -o "browser_download_url.*$ASSET_NAME\"" | cut -d ' ' -f 2)
 
